87b0fc83293c3e80f448eaf2b7bbd97f00104721
imwisdom <kjihye0340@naver.com>
2022-02-07 20:37:56 +0900

    fix: 없는 component에 대한 route 삭제

--- MBFT/src/router/index.ts
+++ MBFT/src/router/index.ts
@@ -12,39 +12,7 @@ const routes: RouteConfig[] = [
     name: 'Home',
     component: Home,
   },
-  {
-    path: '/jihong',
-    name: 'JiHong',
-    component: JiHong,
-  },
-  {
-    path: '/jihong/:mainBoardNum',
-    name: 'Detail',
-    component: Detail,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/wisdom-board',
-    name: 'WisdomMain',
-    component: () => import(/* webpackChunkName: "about" */ '../views/WisdomMain.vue'),
-  },
-  {
-    path: '/wisdom-board/:id',
-    name: 'WisdomBoard',
-    component: () => import('../components/Wisdom/WisdomBoard.vue'),
-  },
-  {
-    path: '/wisdom-board/:id/write',
-    name: 'WisdomWritingPage',
-    component: () => import('../components/Wisdom/WisdomWritingPage.vue'),
-  }
+
 ];
 
 const router = new VueRouter({
