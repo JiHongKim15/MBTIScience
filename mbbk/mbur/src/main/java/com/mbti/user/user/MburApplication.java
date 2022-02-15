f3d06b112877bd19f99ef458a8796bacc0f7e65d
JiHongKim15 <15computer@hanmail.net>
2022-02-16 01:02:34 +0900

    refactoring: 백엔드 구조 변경 및 USER 추가

---
+++ mbbk/mbur/src/main/java/com/mbti/user/user/MburApplication.java
@@ -0,0 +1,13 @@
+package com.mbti.user.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MburApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MburApplication.class, args);
+	}
+
+}
