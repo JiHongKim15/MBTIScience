48fa4822ad6ca76b96cb7f5897bb34b6c59c8fab
JiHongKim15 <15computer@hanmail.net>
2022-02-16 01:02:34 +0900

    refactoring: 백엔드 구조 변경 및 USER 추가

---
+++ mbbk/mbur/src/test/java/com/mbti/user/user/MburApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.user.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MburApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
