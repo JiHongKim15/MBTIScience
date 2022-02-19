2319562d24632a2a39d05310950354b3fdeb5eca
imwisdom <kjihye0340@naver.com>
2022-02-20 01:21:38 +0900

    create: mbti 채팅 프로젝트 생성

---
+++ mbbk/mbct/src/test/java/com/mbti/chat/mbct/MbctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.chat.mbct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
