f1cb0f404c36eb0af2841f263cec3f0abc32f80d
imwisdom <kjihye0340@naver.com>
2022-02-26 17:24:05 +0900

    feature: 채팅 pub/sub 구현

--- mbbk/mbct/src/main/java/com/mbti/chat/mbct/config/RedisConfig.java
+++ mbbk/mbct/src/main/java/com/mbti/chat/mbct/config/RedisConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -14,8 +18,33 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    /*
+     *  redis 서버 연결
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         return new LettuceConnectionFactory(host, port);
+    }
+
+    /*
+     *  redis pub/sub 메시지 처리 Listener
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+    /*
+     *  redis template 설정
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        return redisTemplate;
     }
 }
---
+++ mbbk/mbct/src/main/java/com/mbti/chat/mbct/subscriber/ChatSubscriber.java
@@ -0,0 +1,33 @@
+package com.mbti.chat.mbct.subscriber;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mbti.chat.mbct.domain.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatSubscriber implements MessageListener {
+
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate redisTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern){
+        try{
+            String pubMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+            ChatMessage chatMessage = objectMapper.readValue(pubMessage, ChatMessage.class);
+            messagingTemplate.convertAndSend("/sub/chat/room/"+ chatMessage.getChatRoomId(), chatMessage);
+
+        }catch(Exception e){
+            log.error(e.getMessage());
+        }
+    }
+}
