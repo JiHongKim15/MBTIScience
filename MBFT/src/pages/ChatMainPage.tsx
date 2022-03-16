03479bf03f3b83f52ca7053c7ead31fbe340bb4b
imwisdom <kjihye0340@naver.com>
2022-03-16 23:56:39 +0900

    feature: 채팅 메인 페이지 생성

---
+++ MBFT/src/pages/ChatMainPage.tsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+function ChatMainPage() {
+    return (
+        <div className="ChatMainPage">
+            Chat Main Page
+        </div>
+    );
+}
+
+export default ChatMainPage;
