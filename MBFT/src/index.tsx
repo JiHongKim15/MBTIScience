ee64b45029564f6b2aa52d38cc34a1bb410aa878
imwisdom <kjihye0340@naver.com>
2022-03-16 22:43:54 +0900

    feature: bootstrap 적용

--- MBFT/src/index.tsx
+++ MBFT/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// bootstrap css 가져오기
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
