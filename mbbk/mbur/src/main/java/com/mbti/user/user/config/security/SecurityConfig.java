766996896519d337b5a31376665d4be9382184bf
JiHongKim15 <15computer@hanmail.net>
2022-03-16 22:55:17 +0900

    fix: oauth2 merge error

--- mbbk/mbur/src/main/java/com/mbti/user/user/config/security/SecurityConfig.java
+++ mbbk/mbur/src/main/java/com/mbti/user/user/config/security/SecurityConfig.java
@@ -22,10 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션 disable
                 .and()
                 .authorizeRequests()// URL별 권한 권리
-                .antMatchers("/user").permitAll()
                 .antMatchers("/**","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
-                .antMatchers("/v1/**").hasRole(Role.USER.name()) // /v1/** 은 USER권한만 접근 가능
-                .anyRequest().authenticated() // anyRequest : 설정된 값들 이외 나머지 URL 나타냄, authenticated : 인증된 사용자
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // /api/v1/** 은 USER권한만 접근 가능
+                //.anyRequest().authenticated() // anyRequest : 설정된 값들 이외 나머지 URL 나타냄, authenticated : 인증된 사용자
+                //.anyRequest().permitAll()
                 .and()
                 .logout()
                 .logoutSuccessUrl("/");
