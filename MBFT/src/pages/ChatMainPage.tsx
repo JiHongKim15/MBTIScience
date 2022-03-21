723111f32bf6bcca38a45cfafb869eb09487de52
imwisdom <kjihye0340@naver.com>
2022-03-21 22:02:39 +0900

    feature: Header 생성

--- MBFT/src/pages/ChatMainPage.tsx
+++ MBFT/src/pages/ChatMainPage.tsx
@@ -1,8 +1,10 @@
 import { Button } from "react-bootstrap";
+import ChatHeader from "../components/ChatHeader";
 
 function ChatMainPage() {
     return (
         <div className="ChatMainPage">
+            <ChatHeader></ChatHeader>
             Chat Main Page
         </div>
     );
