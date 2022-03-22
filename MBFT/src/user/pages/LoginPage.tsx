f4b7ea0febaf474a172a6827dfd1c5af0703ae64
JiHongKim15 <15computer@hanmail.net>
2022-03-22 23:18:19 +0900

    feature: google login 연결

--- MBFT/src/user/pages/LoginPage.tsx
+++ MBFT/src/user/pages/LoginPage.tsx
@@ -1,17 +1,37 @@
 import { Button } from "react-bootstrap";
 import LoginButtonGoogle from "../components/NaverLoginButton";
-
+import axios from "axios";
 import {GoogleLoginButton} from 'react-social-login-buttons';
 import NaverLoginButton from "../components/NaverLoginButton";
 
 function LoginPage() {
     const socialLogin = () => {
-        
+        axios.get('http://localhost:8082/oauth2/authorization/google')
+        .then( () => {
+            console.log("로그인 성공");
+        })
+        .catch( (error) => {
+            console.log(error.response);
+        })
+        .finally( () => {
+            console.log("로그인 됐나?");
+        })
     };
     return (
         <div className="LoginPage">
             Login
-            < GoogleLoginButton onClick = { ( ) =>  alert( ' google Button' ) } />
+            < GoogleLoginButton onClick = { ( ) =>  
+            axios.get('http://localhost:8082/oauth2/authorization/google')
+                .then( () => {
+                    console.log("로그인 성공");
+                })
+                .catch( (error) => {
+                    console.log(error.response);
+                })
+                .finally( () => {
+                    console.log("로그인 됐나?");
+                })
+         } />
             < NaverLoginButton onClick = { ( ) =>  alert( ' naver Button' ) } />
         </div>
     );
