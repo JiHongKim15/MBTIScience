a411d228fdff12dda6f6cc252b39e7be2607b8a3
imwisdom <kjihye0340@naver.com>
2022-03-24 21:41:08 +0900

    fix: mbft 대문자를 소문자로 변경

---
+++ mbft/src/user/components/NaverLoginButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createButton } from "react-social-login-buttons";
+
+const config = {
+  text: "Log in with Naver",
+  iconFormat: (name: string) => `fa fa-${name}`,
+  style: { background: "#03C75A" },
+  activeStyle: { background: "#293e69" }
+};
+/** My Facebook login button. */
+const NaverLoginButton = createButton(config);
+
+export default NaverLoginButton;
