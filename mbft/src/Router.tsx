4bbbfa56d33137b4a61118aceecc66fa85c13f58
imwisdom <kjihye0340@naver.com>
2022-03-24 22:46:30 +0900

    feature; 채팅방 리스트 페이지 생성

--- mbft/src/Router.tsx
+++ mbft/src/Router.tsx
@@ -5,13 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ChatMainPage from './chat/pages/ChatMainPage';
 import LoginPage from './user/pages/LoginPage';
 import Home from './common/Home';
+import ChatRoomListPage from './chat/pages/ChatRoomListPage';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Home} exact /> 
-                <Route path="/chat" component={ChatMainPage} />
+                <Route path="/" component={Home} exact />
+                <Route path="/chat" component={ChatMainPage} exact />
+                <Route path="/chat/rooms" component={ChatRoomListPage} />
                 <Route path="/login" component={LoginPage} />
                 <Route component={() => <Redirect to="/" />} />
             </Switch>
