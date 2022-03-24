2f06e018d734f9d6a4a86678d20cd032b7ab93a8
imwisdom <kjihye0340@naver.com>
2022-03-24 21:41:08 +0900

    fix: mbft 대문자를 소문자로 변경

--- mbft/src/user/components/KakaoLoginButton.tsx
+++ mbft/src/user/components/KakaoLoginButton.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { createButton } from "react-social-login-buttons";
+import {createButton} from "react-social-login-buttons";
+import naverIcon from "../../assests/icon/naver_icon.png";
 
 const config = {
   text: "Log in with Naver",
+  icon: naverIcon,
   iconFormat: (name: string) => `fa fa-${name}`,
   style: { background: "#03C75A" },
   activeStyle: { background: "#293e69" }
---
+++ mbft/src/user/pages/LoginPage.tsx
@@ -0,0 +1,40 @@
+import { Button } from "react-bootstrap";
+import LoginButtonGoogle from "../components/NaverLoginButton";
+import axios from "axios";
+import {GoogleLoginButton} from 'react-social-login-buttons';
+import NaverLoginButton from "../components/NaverLoginButton";
+
+function LoginPage() {
+    const socialLogin = () => {
+        axios.get('http://localhost:8082/oauth2/authorization/google')
+        .then( () => {
+            console.log("로그인 성공");
+        })
+        .catch( (error) => {
+            console.log(error.response);
+        })
+        .finally( () => {
+            console.log("로그인 됐나?");
+        })
+    };
+    return (
+        <div className="LoginPage">
+            Login
+            < GoogleLoginButton onClick = { ( ) =>  
+            axios.get('/oauth2/authorization/naver')
+                .then( () => {
+                    console.log("로그인 성공");
+                })
+                .catch( (error) => {
+                    console.log(error.response);
+                })
+                .finally( () => {
+                    console.log("로그인 됐나?");
+                })
+         } />
+            < NaverLoginButton onClick = { ( ) =>  alert( ' naver Button' ) } />
+        </div>
+    );
+}
+
+export default LoginPage;
