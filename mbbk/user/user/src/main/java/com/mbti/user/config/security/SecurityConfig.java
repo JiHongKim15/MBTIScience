6a9d4436916614bb44ec7b2caece3ddfa6cc3b8f
JiHongKim15 <tsopime@gmail.com>
2022-03-25 17:33:10 +0900

    fixing: mbur spring3 -> spring 2.7 이전

---
+++ mbbk/user/user/src/main/java/com/mbti/user/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.mbti.user.config.security;
+
+import com.mbti.user.user.dto.Role;
+import com.mbti.user.user.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션 disable
+                .and()
+                .authorizeRequests()// URL별 권한 권리
+                .antMatchers("/**","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // /api/v1/** 은 USER권한만 접근 가능
+                //.anyRequest().authenticated() // anyRequest : 설정된 값들 이외 나머지 URL 나타냄, authenticated : 인증된 사용자
+                //.anyRequest().permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+    }
+
+}
---
+++ mbbk/user/user/src/main/java/com/mbti/user/user/entity/User.java
@@ -0,0 +1,80 @@
+package com.mbti.user.user.entity;
+
+import com.mbti.user.user.dto.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String email;
+
+    @Column
+    private String id;
+
+    @Column
+    private String pw;
+
+    @Column
+    private String name;
+
+    @Column
+    private String picture;
+
+    @Column
+    private String mbti;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Date lastAccessDate;
+
+    @Column(nullable = false)
+    private Date firstAccessDate;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    @Builder
+    public User(String id, String pw, String name, String email, String picture, Role role) {
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
+
+
+
+
+
