f26e38945851428ebcf7c9e995a44912fbf84af4
JiHongKim15 <15computer@hanmail.net>
2022-04-05 23:26:04 +0900

    fix: 모든 경로에 대해 spring security 허용

--- mbbk/mbur/src/main/java/com/mbti/user/config/security/SecurityConfig.java
+++ mbbk/mbur/src/main/java/com/mbti/user/config/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**",
+                .antMatchers("/**", "/css/**", "/images/**",
                         "/js/**", "/h2-console/**").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.
                         USER.name())
