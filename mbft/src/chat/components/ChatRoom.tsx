0068b2d7b708d6d0f84a866f6dd0c3a43784008b
imwisdom <kjihye0340@naver.com>
2022-04-24 04:10:16 +0900

    feature: prev 화면에서 데이터를 받아 채팅방 헤더 구성

--- mbft/src/chat/components/ChatRoom.tsx
+++ mbft/src/chat/components/ChatRoom.tsx
@@ -1,24 +1,49 @@
 import ChatMessage from './ChatMessage';
 import { Container, Row, Col, ToastContainer, Toast, ToastBody } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+
+interface Ilocation {
+    room: any
+}
 
 function ChatRoom() {
+    const location = useLocation<Ilocation>();
+    const room = location.state.room;
 
     return (
         <Container className="ChatRoom"
-            style
-            ={{
-                position: 'relative',
-                display: 'block',
-                width: '40%',
-                overflowY: "auto",
-                backgroundColor: '#FDF5D2',
-                minHeight: '700px',
-                borderRadius: '10px'
-            }}
+            style={{
+                width: '60%'
+            }}>
+            <Container
+                style={{
+                    fontSize: '30px',
+                    fontFamily: 'IBMPlexSansKR-Medium',
+                    backgroundColor: '#FFC31E',
+                    textAlign: 'left',
+                    display: 'inline',
+                    float: 'left'
+                }}>
+                {/* https://dreamyguy.github.io/react-emojis/ */}
+                <span role="img" aria-label="sparkles">✨</span>
+                <span>{room.chatSubject}</span>
+                <span style={{ float: 'right' }}>{room.mbti}</span>
+            </Container>
+            <Container
+                style
+                ={{
+                    position: 'relative',
+                    display: 'block',
+                    overflowY: "auto",
+                    backgroundColor: '#FDF5D2',
+                    minHeight: '700px',
 
-        >
-            <ChatMessage isFriend={true}></ChatMessage>
-            <ChatMessage isFriend={false}></ChatMessage>
+                }}
+
+            >
+                <ChatMessage isFriend={true}></ChatMessage>
+                <ChatMessage isFriend={false}></ChatMessage>
+            </Container>
         </Container >
     );
 }
