f4b94b19e2dfcf19e196f263b671f4f9e5edb807
imwisdom <kjihye0340@naver.com>
2022-04-24 03:17:59 +0900

    feature: 자신과 타인의 메시지 구별 기능 추가

--- mbft/src/chat/components/ChatRoom.tsx
+++ mbft/src/chat/components/ChatRoom.tsx
@@ -1,11 +1,25 @@
 import ChatMessage from './ChatMessage';
+import { Container, Row, Col, ToastContainer, Toast, ToastBody } from "react-bootstrap";
 
 function ChatRoom() {
 
     return (
-        <div className="ChatRoom">
-            <ChatMessage></ChatMessage>
-        </div >
+        <Container className="ChatRoom"
+            style
+            ={{
+                position: 'relative',
+                display: 'block',
+                width: '40%',
+                overflowY: "auto",
+                backgroundColor: '#FDF5D2',
+                minHeight: '700px',
+                borderRadius: '10px'
+            }}
+
+        >
+            <ChatMessage isFriend={true}></ChatMessage>
+            <ChatMessage isFriend={false}></ChatMessage>
+        </Container >
     );
 }
 
