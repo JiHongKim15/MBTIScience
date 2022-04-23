cd8e0235403067a65fc672c66501825a0a8282c1
imwisdom <kjihye0340@naver.com>
2022-04-24 03:17:59 +0900

    feature: 자신과 타인의 메시지 구별 기능 추가

--- mbft/src/chat/components/ChatMessage.tsx
+++ mbft/src/chat/components/ChatMessage.tsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
+import '../../App.css';
 
 //https://projects.verou.me/bubbly/
-function ChatMessage() {
-
-    const MessageStyle = styled.div`
+const MessageStyle = styled.div`
         
-        position: absolute;
-        background: #a58af0;
+        position: sticky;
+        background: ${props => (props.isFriend ? '#a58af0' : '#51d5e1')};
         border-radius: .4em;
         padding : 10px;
+        max-width : 300px;
+        text-align: left;
+        ${props => (props.isFriend ? 'float: left;' : 'float: right;')}
+        font-size: 20px;
+        font-family: IBMPlexSansKR-Medium;
+        
 
         &:after {
             content: '';
             position: absolute;
             bottom: 0;
-            left: 30%;
+            ${props => (props.isFriend ? 'left: 30%;' : 'right: 30%;')}
             width: 0;
             height: 0;
             border: 23px solid transparent;
-            border-top-color: #a58af0;
+            border-top-color: ${props => (props.isFriend ? '#a58af0' : '#51d5e1')};
             border-bottom: 0;
-            border-left: 0;
-            margin-left: -11.5px;
+            ${props => (props.isFriend ? 'border-left: 0;' : 'border-right: 0;')}
+            ${props => (props.isFriend ? 'margin-left: -11.5px;' : 'margin-right: -11.5px;')}
             margin-bottom: -23px;
         }
     `;
+
+const DivStyle = styled.div`
+        display: block;
+        text-align: left;
+        margin: 30px;
+`;
+function ChatMessage(props) {
+
     return (
         <div className="ChatMessage">
-            <MessageStyle>
-                Hello World!
-            </MessageStyle>
-        </div>
+            <DivStyle>
+                {
+                    props.isFriend &&
+                    <div style={{ fontFamily: 'IBMPlexSansKR-Medium' }}>지혜</div>
+                }
+                <MessageStyle isFriend={props.isFriend}>
+                    안녕안녕
+                </MessageStyle>
+            </DivStyle>
+        </div >
     );
 }
 
