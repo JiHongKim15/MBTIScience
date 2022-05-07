05fcedf416275551025a5d70a3e88a47f6405871
JiHongKim15 <15computer@hanmail.net>
2022-05-08 03:46:32 +0900

    fix: 충돌 해결

--- mbbk/mbur/src/main/java/com/mbti/user/user/service/UserService.java
+++ mbbk/mbur/src/main/java/com/mbti/user/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.mbti.user.user.service;
 
 import com.mbti.user.exception.BusinessException;
+import com.mbti.user.user.dto.UserDto;
+import com.mbti.user.user.entity.UserMapper;
 import com.mbti.user.user.repository.UserRepository;
 import com.mbti.user.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +19,25 @@ public class UserService{
 
     private final UserRepository userRepository;
 
+    /**
+     * Email 정보로 User 정보를 리턴한다.
+     * @param email
+     * @return
+     */
     @Transactional(readOnly = true)
     public User retrieveUserByEmail(String email){
         Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email)
                 .orElseThrow(() -> new BusinessException("사용자 정보를 가져오는 도중 오류가 발생하였습니다.")));
         return user.get();
+    }
+
+    /**
+     * User Mbti 정보를 업데이트 한다.
+     * @param  user
+     * @return
+     */
+    public User updateUserMbti(User user) {
+        userRepository.save(user);
+        return user;
     }
 }
